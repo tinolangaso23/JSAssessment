f23d31c2d04dad93d27ed686ebf32abb507bf620
Regie Prieto <131588448+tinolangaso23@users.noreply.github.com>
2023-04-23 23:11:26 +0800

    Add files via upload

---
+++ PrietoETH.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+    // public variables here
+    string public name = "CLASS";
+    string public abbrv = "CLS";
+    uint public supply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public blnc;
+
+    // mint function
+    function mint(address add, uint val) public{
+        supply += val;
+        blnc[add] += val;
+    }
+    // burn function
+    function burn(address add, uint val) public{
+        if(blnc[add] >= val){
+            supply -= val;
+            blnc[add] -= val;
+        }
+    } 
+
+}
